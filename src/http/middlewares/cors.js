86d7ad1c6fc24504de9c37f162bfa407e4ddc44e
David <joose3mil@gmail.com>
2025-10-27 13:11:20 -0400

    feat: add new valid metods

--- src/http/middlewares/cors.js
+++ src/http/middlewares/cors.js
@@ -1,6 +1,6 @@
 function cors(request, response, next) {
   response.header('Access-Control-Allow-Origin', '*');
-  response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
   response.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
 
   if (request.method === 'OPTIONS') {
